05f1792c3ffb8c90b7942d7fdd5e433454ab19b0
Ivan Caviedes <ivancaviedes99@gmail.com>
2023-10-07 00:55:44 -0500

    creando guard auth

--- src/app/core/auth/guards/auth.guard.ts
+++ src/app/core/auth/guards/auth.guard.ts
@@ -1,14 +1,51 @@
-import { Injectable } from '@angular/core';
-import { CanMatch, Route, UrlSegment, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Injectable, inject } from '@angular/core';
+import {
+  CanMatchFn,
+  Route,
+  Router,
+  UrlSegment,
+  UrlTree,
+} from '@angular/router';
+import { Observable, of, switchMap } from 'rxjs';
+import { AuthService } from '../auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuard implements CanMatch {
+class AuthGuard {
+  constructor(private _authService: AuthService, private _router: Router) {}
+
   canMatch(
     route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    segments: UrlSegment[]
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.check(segments);
+  }
+
+  check(segments: UrlSegment[]): Observable<boolean | UrlTree> {
+    return this._authService.checkAuth().pipe(
+      switchMap((authenticated) => {
+        if (!authenticated) {
+          const redirectURL = `/${segments.join('/')}`;
+          const urlTree = this._router.parseUrl(
+            `sign-in?redirectURL=${redirectURL}`
+          );
+
+          return of(urlTree);
+        }
+        return of(true);
+      })
+    );
   }
 }
+
+export const IsAuthGuard: CanMatchFn = (
+  route: Route,
+  segments: UrlSegment[]
+) => {
+  return inject(AuthGuard).canMatch(route, segments);
+};
