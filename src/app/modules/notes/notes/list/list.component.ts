d31ec989d245f6d35b6447a93f3c55a03440b02c
Ivan Caviedes <36604636+IvanCaviedes@users.noreply.github.com>
2023-10-07 14:26:10 -0500

    creando vista de notes list (#9)

--- src/app/modules/notes/notes/list/list.component.ts
+++ src/app/modules/notes/notes/list/list.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss'],
 })
 export class NotesListComponent implements OnInit {
+  listArr = new Array(15).fill(0);
+
   constructor() {}
 
   ngOnInit() {}
+
+  getColorClass(index: number): string {
+    const colors = [
+      'bg-blue-300',
+      'bg-orange-300',
+      'bg-pink-300',
+      'bg-teal-300',
+    ]; // Nombres de las clases CSS para cada color
+    const colorIndex = index % colors.length; // Calcula el índice del color usando el operador módulo
+    return colors[colorIndex];
+  }
 }
