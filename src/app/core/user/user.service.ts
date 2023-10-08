15c91fccdd6ced1eb0545aae9e18d904890edc63
Ivan Caviedes <ivancaviedes99@gmail.com>
2023-10-07 23:27:47 -0500

    creando servios de auth y user

---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { User } from './user.types';
+import { Observable, ReplaySubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private _user: ReplaySubject<User> = new ReplaySubject<User>(1);
+
+  constructor() {}
+
+  set user(value: User) {
+    // Store the value
+    this._user.next(value);
+  }
+
+  get user$(): Observable<User> {
+    return this._user.asObservable();
+  }
+}
---
+++ src/app/core/user/user.types.ts
@@ -0,0 +1,7 @@
+export interface User {
+  id: string;
+  profile: string;
+  username: string;
+  password: string;
+  country: string;
+}
