19081ff995b8713b86dab0203afe94aeac496233
Ivan Caviedes <ivancaviedes99@gmail.com>
2023-10-07 23:29:57 -0500

    creando page sign up con servicios

--- src/app/modules/auth/sign-up/sign-up.component.ts
+++ src/app/modules/auth/sign-up/sign-up.component.ts
@@ -1,14 +1,95 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  NgForm,
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountryService } from '@ntt-data/services/country/country.service';
+import { Country } from '@ntt-data/services/country/country.types';
+import { AuthService } from 'app/core/auth/auth.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
 })
-export class SignUpComponent  implements OnInit {
+export class SignUpComponent {
+  signUpForm: UntypedFormGroup;
+  resultSearch: boolean = false;
+  arrayval = new Array(5).fill(0);
+  countries: Country[] = [];
+  showAlert: boolean = false;
+  @ViewChild('signUpNgForm') signInNgForm!: NgForm;
 
-  constructor() { }
+  showPassword: boolean = false;
 
-  ngOnInit() {}
+  alert: { message: string } = {
+    message: '',
+  };
 
+  constructor(
+    private _formBuilder: UntypedFormBuilder,
+    private _countryService: CountryService,
+    private _authService: AuthService,
+    private _activatedRoute: ActivatedRoute,
+    private _router: Router
+  ) {
+    this.signUpForm = this._formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', Validators.required],
+      country: ['Colombia', [Validators.required, Validators.minLength(3)]],
+    });
+  }
+  searchCountry(searchTerm: string): void {
+    if (searchTerm.length === 0) {
+      this.resultSearch = false;
+    }
+    if (searchTerm.length >= 2) {
+      const result = this._countryService.searchCountries(searchTerm);
+      if (result) {
+        this.resultSearch = true;
+        this.countries = result;
+      }
+    }
+  }
+  selectCountry(country: string): void {
+    this.signUpForm.patchValue({
+      country: country,
+    });
+    this.resultSearch = false;
+  }
+
+  signUp(): void {
+    if (this.signUpForm.invalid) {
+      return;
+    }
+    this.signUpForm.disable();
+
+    this._authService.signUp(this.signUpForm.value).subscribe(
+      () => {
+        const redirectURL =
+          this._activatedRoute.snapshot.queryParamMap.get('redirectURL') ||
+          '/signed-in-redirect';
+        this._router.navigateByUrl(redirectURL);
+        this.selectCountry('Colombia');
+        this.signUpForm.enable();
+        this.signInNgForm.resetForm();
+      },
+      (response) => {
+        this.signUpForm.enable();
+        this.signInNgForm.resetForm();
+        this.alert = {
+          message: 'The user has already registered',
+        };
+        this.selectCountry('Colombia');
+        this.showAlert = true;
+      }
+    );
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 }
